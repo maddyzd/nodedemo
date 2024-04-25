8bf8bb7fc5a0f53bd272f36ddb0570772ae7dbdc
maddyzd <38566228+maddyzd@users.noreply.github.com>
2024-04-24 22:48:33 -0400

    Update node.js

--- node.js
+++ node.js
@@ -1,15 +1,28 @@
+const http = require('http');
 const MongoClient = require('mongodb').MongoClient;
-const connStr= "mongodb+srv://mdumon:mydb123@cluster0.rvujnyd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-	  
-console.log('hey')
-  MongoClient.connect(connStr, function(err, db) {
-    
-  if(err) { console.log(err); }
-  else {
-    var dbo = db.db("Stock");
-	  var collection = dbo.collection('PublicCompanies');
-    console.log("Success!");
-	  db.close();
-  }
-});
 
+const port = process.env.PORT || 3000;
+const connStr = "mongodb+srv://mdumon:mydb123@cluster0.rvujnyd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+console.log("This goes to the console window");
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("<h2>Hello World</h2>");
+    res.write("Success! This app is deployed online");
+    res.end();
+}).listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  
+    console.log('Connecting to MongoDB...');
+    MongoClient.connect(connStr, function(err, db) {
+        if (err) { 
+            console.log(err);
+        } else {
+            const dbo = db.db("Stock");
+            const collection = dbo.collection('PublicCompanies');
+            console.log("Connected to MongoDB!");
+            db.close();
+        }
+    });
+});
